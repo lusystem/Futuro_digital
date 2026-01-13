a7fefcf51a00daf0c779c5a4ce076ff279c35659
Andy <andy.lukitaa@gmail.com>
2026-01-13 18:32:44 -0300

    teste escola

--- conf/database.py
+++ conf/database.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    # Não sobrescrever uma configuração já existente (por exemplo em testes).
     default_uri = 'postgresql+psycopg2://postgres:123@localhost/Banco de dados - EduGestao'
     app.config.setdefault('SQLALCHEMY_DATABASE_URI', default_uri)
 
