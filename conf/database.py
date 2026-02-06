d647d2d8b90323fb27664283c073b9f3f59d08bb
Andy <andy.lukitaa@gmail.com>
2026-02-05 23:48:50 -0300

    database teste

--- conf/database.py
+++ conf/database.py
@@ -1,10 +1,18 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 def init_db(app):
-    default_uri = 'postgresql+psycopg2://postgres:123@localhost/edugesto'
-    app.config.setdefault('SQLALCHEMY_DATABASE_URI', default_uri)
-    app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    database_url = os.environ.get("DATABASE_URL")
+
+    if not database_url:
+        database_url = "postgresql+psycopg2://postgres:123@localhost/edugesto"
+
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
